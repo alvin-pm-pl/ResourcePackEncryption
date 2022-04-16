399d73ebd048dfb6989c92eb1f8602ca39ae9c55
alvin0319 <alvin7810@naver.com>
2022-04-16 09:23:47 +0900

    Initial commit

---
+++ src/alvin0319/ResourcePackEncryption/Loader.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace alvin0319\ResourcePackEncryption;
+
+use pocketmine\event\EventPriority;
+use pocketmine\event\server\DataPacketSendEvent;
+use pocketmine\network\mcpe\protocol\ResourcePacksInfoPacket;
+use pocketmine\plugin\PluginBase;
+
+final class Loader extends PluginBase{
+
+	/** @var string[] */
+	private array $encryptionKeys = [];
+
+	protected function onEnable() : void{
+		$this->saveDefaultConfig();
+		foreach($this->getServer()->getResourcePackManager()->getResourceStack() as $resourcePack){
+			$uuid = $resourcePack->getPackId();
+			if($this->getConfig()->getNested("resource-packs.{$uuid}", "") !== ""){
+				$encryptionKey = $this->getConfig()->getNested("resource-packs.{$uuid}");
+				$this->encryptionKeys[$uuid] = $encryptionKey;
+				$this->getLogger()->debug("Loaded encryption key for resource pack $uuid");
+			}
+		}
+		$this->getServer()->getPluginManager()->registerEvent(DataPacketSendEvent::class, function(DataPacketSendEvent $event) : void{
+			$packets = $event->getPackets();
+			foreach($packets as $packet){
+				if($packet instanceof ResourcePacksInfoPacket){
+					foreach($packet->resourcePackEntries as $index => $entry){
+						if(isset($this->encryptionKeys[$entry->getPackId()])){
+							$contentId = $this->encryptionKeys[$entry->getPackId()];
+							(function() use ($entry, $contentId) : void{
+								/* @noinspection PhpUndefinedFieldInspection */
+								$this->contentId = $entry->getPackId();
+								/* @noinspection PhpUndefinedFieldInspection */
+								$this->encryptionKey = $contentId;
+							})->call($entry);
+						}
+					}
+				}
+			}
+		}, EventPriority::HIGHEST, $this);
+	}
+}
